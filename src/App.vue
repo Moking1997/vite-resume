cc4e35af3fd99dd1fab6da31b0815769f38823cc
Moking1997 <moking1997@icloud.com>
2020-12-08 23:17:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <a-button type="primary">测试</a-button>
+</template>
+
+<script lang='ts'>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
